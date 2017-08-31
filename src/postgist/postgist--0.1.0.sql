93cd065c6c5003dd58f2ea6b93c761f0d8a8053c
Fabiana Zioti <fabi_zioti@hotmail.com>
2017-08-30 22:22:24 -0300

    test for LWGEOM

--- src/postgist/postgist--0.1.0.sql
+++ src/postgist/postgist--0.1.0.sql
@@ -3,3 +3,7 @@
 --
 \echo Use "CREATE EXTENSION postgist" to load this file. \quit
 
+CREATE OR REPLACE FUNCTION create_trj_element(timestamp, geometry)
+    RETURNS cstring
+    AS 'MODULE_PATHNAME','create_trajectory_elem'
+    LANGUAGE C IMMUTABLE STRICT;
